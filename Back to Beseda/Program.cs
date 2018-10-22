6ec2f272c647706574689410a6804f520059fca6
Сергей Карманов <ppoocpel8888@outlook.com>
2018-10-22 21:49:47 +0300

    Добавлена двух-факторная авторизация, авторизация по api токену.

--- Back to Beseda/Program.cs
+++ Back to Beseda/Program.cs
@@ -14,85 +14,117 @@ namespace Back_to_Beseda
             string Login;
             string Password;
             ulong ID;
-            long UserID;
-            string ChatID;
             string VKId;
 
+            Console.WriteLine("Возращения пользователя в беседу by serega404");
+            Console.WriteLine();
             while (true)
             {
-                Console.WriteLine("Возращения пользователя в беседу by serega404");
-
-                Console.WriteLine("Если пропустить ввод ID, то автоматически будет выбрано приложение Back to Beseda");
-
-                try
+                Console.WriteLine("Аторизация через Api = 'y', через Login/Pass/Id = 'n'");
+                Console.Write("Выберите способ авторизации (по умолчанию y): ");
+                if (Console.ReadLine() == "n" )
                 {
-                    Console.Write("Введите Id приложения: ");
-                    VKId = Console.ReadLine();
-
-                    if (VKId == "" || VKId == " ")
+                    try
                     {
-                        ID = ulong.Parse("6720120");
-                    }
-                    else
-                    {
-                        ID = ulong.Parse(VKId);
-                    }
+                        Console.WriteLine("Если пропустить ввод ID, то автоматически будет выбрано приложение Back to Beseda (6720120)");
+                        Console.Write("Введите Id приложения: ");
+                        VKId = Console.ReadLine();
 
-                    Console.Write("Введите логин: ");
-                    Login = Console.ReadLine();
-
-                    Console.Write("Введите пароль: ");
-                    Password = Console.ReadLine();
-
-                    Console.Clear();
-
-                    if (Auth(Login, Password, ID))
-                    {
-                        while (true)
+                        if (VKId == "" || VKId == " ")
                         {
-                            try
-                            {
-                                Console.WriteLine("В ссылке беседы \"...&sel=dXX\", XX = id беседы ");
+                            ID = ulong.Parse("6720120");
+                        }
+                        else
+                        {
+                            ID = ulong.Parse(VKId);
+                        }
 
-                                Console.Write("ID беседы: ");
-                                ChatID = Console.ReadLine();
+                        Console.Write("Введите логин: ");
+                        Login = Console.ReadLine();
 
-                                Console.WriteLine("В ссылке на страницу пользователя \".../idXXXXXXXXX\",где иксы это id пользователя ");
-                                Console.WriteLine("Или можно открыть картинку пользователя и там в ссылке \"...photoXXXXXXXXX_...\",где иксы это id пользователя ");
+                        Console.Write("Введите пароль: ");
+                        Password = Console.ReadLine();
 
-                                Console.Write("ID пользователя: ");
-                                UserID = long.Parse(Console.ReadLine());
+                        Console.Clear();
 
-                                vkapi.Messages.RemoveChatUser(ulong.Parse(ChatID), UserID);
-                                Console.Clear();
-                                try
-                                {
-                                    vkapi.Messages.AddChatUser(long.Parse(ChatID), UserID);
-                                    Console.WriteLine("Пользователь с id " + UserID + " успешно возращён в беседу!");
-                                }
-                                catch
-                                {
-                                    Console.WriteLine("Не удалось вернуть пользователя!");
-                                }
-                            }
-                            catch
-                            {
-                                Console.Clear();
-                                Console.WriteLine("Не удалось исключить пользователя из беседы!");
-                            }
+                        if (Auth(Login, Password, ID))
+                        {
+                            Back();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Не удалось авторизоваться!");
                         }
                     }
-                    else
+                    catch
                     {
-                        Console.WriteLine("Не удалось авторизоваться!");
+                        Console.WriteLine("Ошибка ввода данных авторизации приложения");
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        Console.Write("Введите Api токен: ");
+                        if (Api_Auth(Console.ReadLine()))
+                        {
+                            Console.Clear();
+                            Back();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Не удалось авторизоваться!");
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Ошибка ввода данных авторизации приложения");
+                    }
+                }
+                    
+                     
+
+                
+            }
+        }
+
+        static void Back()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("В ссылке беседы \"...&sel=dXX\", XX = id беседы ");
+
+                    Console.Write("ID беседы: ");
+                    string ChatID = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.WriteLine("В ссылке на страницу пользователя \".../idXXXXXXXXX\",где иксы это id пользователя ");
+                    Console.WriteLine("Или можно открыть картинку пользователя и там в ссылке \"...photoXXXXXXXXX_...\",где иксы это id пользователя ");
+
+                    Console.Write("ID пользователя: ");
+                    long UserID = long.Parse(Console.ReadLine());
+
+                    vkapi.Messages.RemoveChatUser(ulong.Parse(ChatID), UserID);
+                    Console.Clear();
+                    try
+                    {
+                        vkapi.Messages.AddChatUser(long.Parse(ChatID), UserID);
+                        Console.WriteLine("Пользователь с id " + UserID + " успешно возращён в беседу!");
+                        Console.WriteLine();
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Не удалось вернуть пользователя!");
+                        Console.WriteLine();
                     }
                 }
                 catch
                 {
-                    Console.WriteLine("Ошибка ввода данных авторизации приложения");
-                }        
-
-                
+                    Console.Clear();
+                    Console.WriteLine("Не удалось исключить пользователя из беседы!");
+                    Console.WriteLine();
+                }
             }
         }
 
@@ -105,7 +137,29 @@ namespace Back_to_Beseda
                     Login = Login,
                     Password = Password,
                     Settings = Settings.All,
-                    ApplicationId = ID
+                    ApplicationId = ID,
+                    TwoFactorAuthorization = () =>
+                    {
+                        Console.WriteLine("Впишите код двух-этапной авторизации или пропустите если не используете её");
+                        Console.Write("Enter Code: ");
+                        return Console.ReadLine();
+                    }
+                });
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static bool Api_Auth(string token)
+        {
+            try
+            {
+                vkapi.Authorize(new ApiAuthParams
+                {
+                    AccessToken = token
                 });
                 return true;
             }
